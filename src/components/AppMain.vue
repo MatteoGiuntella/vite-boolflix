1b7bd53117bdae9a6bf69931ccd09ad2ad96e953
MatteoGiuntella <matteo.giuntella@virgilio.it>
2024-01-25 17:46:15 +0100

    problema con le bandiere

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -4,35 +4,44 @@ import {store} from '../store.js'
 export default {
     data() {
         return {
-            store
+            store,
         };
     },
     methods: {
-
+        getFlag(){
+            this.ele.original_language = this.ele.original_language.toUpperCase();
+            if (this.ele.original_language == 'EN') {
+                this.ele.original_language = 'GB'
+            }
+        }
+        
+    },
+    mounted(){
+        this.getFlag()
     }
 }
 </script>
 
 <template>
     <main>
-       <div>
-        <h2>film</h2>
-            <ul v-for="(elem) in store.movies">
-                <li>Title: {{ elem.title }}</li>
-                <li>Original title: {{ elem.original_title }}</li>
-                <li>Vote: {{ elem.vote_average }}</li>
-                <li>Original language: {{ elem.original_language }}</li>
-                <li>Overview: {{ elem.overview }}</li>
+       <div class="container">
+        <h2>Film</h2>
+            <ul v-for="(ele) in store.movies">
+                <li>Title: {{ ele.title }} </li>
+                <li>Original title: {{ ele.original_title }} </li>
+                <li>Vote: {{ ele.vote_average }} </li>
+                <li>Original language: {{ ele.original_language }} </li>
+                <li>Overview: {{ ele.overview }} </li>
+                <img :src="'https://flagsapi.com/'+ ele.original_language + '/flat/64.png'" alt="">
             </ul>
-       </div>
-       <div>
-        <h2>serie</h2>
-            <ul v-for="(elem,i) in store.series">
-                <li>Title: {{ elem.title }}</li>
-                <li>Original title: {{ elem.original_title }}</li>
-                <li>Vote: {{ elem.vote_average }}</li>
-                <li>Original language: {{ elem.original_language }}</li>
-                <li>Overview: {{ elem.overview }}</li>
+        <h2>Serie</h2>
+            <ul v-for="(elem) in store.series">
+                <li>Title: {{ elem.name }} </li>
+                <li>Original title: {{ elem.original_name }} </li>
+                <li>Vote: {{ elem.vote_average }} </li>
+                <li>Original language: {{ elem.original_language }} </li>
+                <li>Overview: {{ elem.overview }} </li>
+                <img :src="'https://flagsapi.com/'+ elem.original_language + '/flat/64.png'" alt="">
             </ul>
        </div>
     </main>
