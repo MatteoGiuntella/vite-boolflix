fae27195ea7468a372aaee230735853d69056b27
MatteoGiuntella <matteo.giuntella@virgilio.it>
2024-01-25 16:27:36 +0100

    inizio milenstone 2

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,25 +7,51 @@ export default {
       store,
     };
   },
-  methods: {},
+  methods: {GetFilm (){
+    axios.get('https://api.themoviedb.org/3/search/movie?api_key=83c77370d4de9a06185d3fa334f6fcc7',{
+                params :{
+                    query:this.store.searchText
+                }
+            })
+            .then((response) =>{
+
+                this.store.movies = response.data.results
+
+            console.log('film',this.store.movies)
+            });
+
+            axios.get('https://api.themoviedb.org/3/search/tv?api_key=83c77370d4de9a06185d3fa334f6fcc7',{
+                params :{
+                    query:this.store.searchText
+                }
+            })
+            .then((response) =>{
+
+                this.store.series = response.data.results
+                console.log('response',response.data)
+                console.log('series',this.store.series)
+            });            
+        },
+      },
 };
 </script>
 
 <template>
   <header>
-    <div class="searchbar d-flex justify-content-between container-fluid bg-black ">
+    <div class="search-bar d-flex justify-content-between container-fluid bg-black ">
       <div class="container-img">
         <img src="../assets/img/BrandAssets_Logos_01-Wordmark.jpg" alt="" />
       </div>
       <div class="bar">
         <form class="d-flex" role="search">
           <input
+            v-model="store.searchText"
             class="form-control me-2"
             type="search"
             placeholder="Search..."
             aria-label="Search"
           />
-          <button class="btn btn-outline-success" type="submit">Search</button>
+          <button @click="GetFilm()" class="btn btn-outline-success" type="button">Search</button>
         </form>
       </div>
     </div>
@@ -33,7 +59,7 @@ export default {
 </template>
 
 <style lang="scss" scoped>
-.searchbar{
+.search-bar{
     padding: 20px;
 }
 .container-img {
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -16,12 +16,24 @@ export default {
 <template>
     <main>
        <div>
-        <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-        </ul>
+        <h2>film</h2>
+            <ul v-for="(elem) in store.movies">
+                <li>Title: {{ elem.title }}</li>
+                <li>Original title: {{ elem.original_title }}</li>
+                <li>Vote: {{ elem.vote_average }}</li>
+                <li>Original language: {{ elem.original_language }}</li>
+                <li>Overview: {{ elem.overview }}</li>
+            </ul>
+       </div>
+       <div>
+        <h2>serie</h2>
+            <ul v-for="(elem,i) in store.series">
+                <li>Title: {{ elem.title }}</li>
+                <li>Original title: {{ elem.original_title }}</li>
+                <li>Vote: {{ elem.vote_average }}</li>
+                <li>Original language: {{ elem.original_language }}</li>
+                <li>Overview: {{ elem.overview }}</li>
+            </ul>
        </div>
     </main>
 </template>
