1837727a044a9bce57b607ebd67c7043bed4568c
MatteoGiuntella <matteo.giuntella@virgilio.it>
2024-01-25 11:43:44 +0100

    .

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,23 +1,39 @@
 <script>
-import axios from 'axios';
-import {store} from './store.js'
+import axios from "axios";
+import { store } from "./store.js";
 export default {
-    data() {
-        return {
-            store
-        };
-    },
-    methods: {
-
-    }
-}
+  data() {
+    return {
+      store,
+    };
+  },
+  methods: {},
+};
 </script>
 
 <template>
-    <header>
-       
-    </header>
+  <header>
+    <div class="navbar">
+      <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+          <div class="container-img">
+            <img src="" alt="">
+          </div>
+          <form class="d-flex" role="search">
+            <input
+              class="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button class="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
+    </div>
+  </header>
 </template>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
