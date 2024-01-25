99c3f65f7808fc5a4848fcfde0955d1d714021b8
MatteoGiuntella <matteo.giuntella@virgilio.it>
2024-01-25 11:59:48 +0100

    creata navbar and header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,6 @@
 <script>
 import axios from "axios";
-import { store } from "./store.js";
+import { store } from "../store.js";
 export default {
   data() {
     return {
@@ -13,27 +13,35 @@ export default {
 
 <template>
   <header>
-    <div class="navbar">
-      <nav class="navbar bg-body-tertiary">
-        <div class="container-fluid">
-          <div class="container-img">
-            <img src="" alt="">
-          </div>
-          <form class="d-flex" role="search">
-            <input
-              class="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button class="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
-        </div>
-      </nav>
+    <div class="searchbar d-flex justify-content-between container-fluid bg-black ">
+      <div class="container-img">
+        <img src="../assets/img/BrandAssets_Logos_01-Wordmark.jpg" alt="" />
+      </div>
+      <div class="bar">
+        <form class="d-flex" role="search">
+          <input
+            class="form-control me-2"
+            type="search"
+            placeholder="Search..."
+            aria-label="Search"
+          />
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
     </div>
   </header>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.searchbar{
+    padding: 20px;
+}
+.container-img {
+  width: 200px;
+  height: 50%;
+  img {
+    height: 50px;
+    width: 100%;
+  }
+}
+</style>
